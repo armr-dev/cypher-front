fca18e398150190b68343fe4b95e0dfca7a241bd
armr-dev <andrermr.dev@gmail.com>
2021-06-19 14:32:36 -0400

    :sparkles: add TextArea component

---
+++ src/Components/TextArea.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let value;
+  export let placeholder;
+  export let label;
+</script>
+
+<label>
+  <textarea cols="50" rows="8" bind:value={value}></textarea>
+</label>
+
+<style>
+  input {
+    height: 250px;
+  }
+</style>
