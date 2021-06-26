f327b9606c338ca6b6bb64e4bd672ab8eace4a6c
armr-dev <andrermr.dev@gmail.com>
2021-06-26 13:04:03 -0400

    :sparkles: add label to TextArea

--- src/Components/TextArea.svelte
+++ src/Components/TextArea.svelte
@@ -1,14 +1,29 @@
 <script>
   export let value;
   export let placeholder;
-  export let label;
+  export let label = 'Text';
 </script>
 
-<label>
-  <textarea cols="50" rows="8" bind:value={value}></textarea>
-</label>
+<div class="container">
+  <span>{label}</span>
+  <label>
+    <textarea cols="50" rows="8" bind:value={value}></textarea>
+  </label>
+</div>
 
 <style>
+  .container {
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+  }
+
+  span {
+    margin-bottom: 0.25rem;
+    color: #333333;
+    font-weight: 300;
+  }
+
   input {
     height: 250px;
   }
