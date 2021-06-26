16aee1146977e69442a8b0bbd05270c299109649
armr-dev <andrermr.dev@gmail.com>
2021-06-26 12:49:52 -0400

    :sparkles: add a Select Component

---
+++ src/Components/Select.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let selectedValue;
+  export let options;
+</script>
+
+<div class="container">
+    <span>
+        bacon
+    </span>
+    <label>
+        <select bind:value={selectedValue}>
+            {#each options as option}
+                <option value={option.id}>
+                    {option.text}
+                </option>
+            {/each}
+        </select>
+    </label>
+</div>
+
+<style>
+    .container {
+        width: fit-content;
+        display: flex;
+        flex-direction: column;
+    }
+
+    span {
+        margin-bottom: 0.25rem;
+        text-transform: capitalize;
+        color: #333333;
+        font-weight: 300;
+    }
+
+    select {
+        min-width: 250px;
+        background-color: white;
+    }
+</style>
