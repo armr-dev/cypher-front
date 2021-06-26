f657e18fe1d400c8fce4ed3026f3081d4f096297
armr-dev <andrermr.dev@gmail.com>
2021-06-26 13:43:34 -0400

    :sparkles: add a Text component

---
+++ src/Components/Text.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let label = '';
+    export let text = ''
+</script>
+
+<main>
+    <div class="container">
+        <span>{label}</span>
+        <div class="p-wrapper">
+            <p>{text}</p>
+        </div>
+    </div>
+</main>
+
+<style>
+    .container {
+        width: 510px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    span {
+        margin-bottom: 0.25rem;
+        color: #333333;
+        font-weight: 300;
+    }
+
+    .p-wrapper {
+        font-family: Consolas, monaco, monospace;
+        padding: 0.5rem;
+        border: 1px #999999 dashed;
+    }
+
+    p {
+        margin: 0;
+        color: #333333;
+        font-weight: 300;
+    }
+</style>
